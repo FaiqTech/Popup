bcaad07cd0004086bf7784935fd8e0dac595c9bd
FaiqTech <hesenovfaiq@icloud.com>
2023-12-08 04:13:35 +0400

    first commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,51 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const contactBtn = document.querySelector('.contact-btn');
-    const overlay = document.querySelector('.overlay');
-    const popup = document.querySelector('.information__center');
-    const closeBtn = document.querySelector('.information__center_content_btn');
-  
-    // Popup'ı gösteren fonksiyon
-    function showPopup() {
+  const contactBtn = document.querySelector('.contact-btn');
+  const overlay = document.querySelector('.overlay');
+  const popup = document.querySelector('.information__center');
+  const closeBtn = document.querySelector('.information__center_content_btn');
+  const callorderPopup = document.querySelector('.callorder');
+  const onclickDiv = document.querySelector('.onclick');
+
+  function showPopup() {
       overlay.classList.remove('hidden');
       popup.classList.remove('hidden');
-    }
-  
-    // Popup'ı gizleyen fonksiyon
-    function hidePopup() {
+  }
+
+  function hidePopup() {
       overlay.classList.add('hidden');
       popup.classList.add('hidden');
-    }
-  
-    // İletişim butonu için olay dinleyicisi
-    contactBtn.addEventListener('click', showPopup);
-  
-    // Popup içindeki kapatma butonu için olay dinleyicisi
-    closeBtn.addEventListener('click', hidePopup);
-  
-    // Overlay'e tıklanınca (popup dışına tıklanınca) popup'ı kapatma için olay dinleyicisi
-    overlay.addEventListener('click', function (event) {
-      if (event.target === overlay) {
-        hidePopup();
-      }
-    });
+  }
+
+  function showCallorderPopup() {
+      overlay.classList.remove('hidden');
+      callorderPopup.classList.remove('hidden');
+  }
+
+  function hideCallorderPopup() {
+      overlay.classList.add('hidden');
+      callorderPopup.classList.add('hidden');
+  }
+
+  contactBtn.addEventListener('click', function () {
+      showPopup();
+      hideCallorderPopup();
   });
-  
+
+  closeBtn.addEventListener('click', hidePopup);
+  overlay.addEventListener('click', function (event) {
+      if (event.target === overlay) {
+          hidePopup();
+          hideCallorderPopup();
+      }
+  });
+
+  onclickDiv.addEventListener('click', function () {
+      showCallorderPopup();
+      hidePopup();
+  });
+
+  const closeCallorderBtn = document.querySelector('.callorder__title .close-btn');
+  closeCallorderBtn.addEventListener('click', function () {
+      hideCallorderPopup();
+  });
+});
